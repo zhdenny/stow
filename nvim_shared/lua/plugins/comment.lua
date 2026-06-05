02caec655f8850d85f4e811c690d4511437c442e
zhdenny <dennyhz@gmail.com>
2026-06-05 08:05:43 -0400

    fix comment issue on .env files

--- nvim_shared/lua/plugins/comment.lua
+++ nvim_shared/lua/plugins/comment.lua
@@ -8,6 +8,12 @@ return {
 				---Extra mapping; `gco`, `gcO`, `gcA`
 				extra = false,
 			},
+			pre_hook = function(ctx)
+				local bufname = vim.api.nvim_buf_get_name(0)
+				if bufname:match("%.env$") or bufname:match("%.env%.") or vim.bo.commentstring == "" or vim.bo.commentstring == nil then
+					return "# %s"
+				end
+			end,
 		})
 	end,
 }
