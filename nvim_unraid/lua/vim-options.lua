b2ede3c776f0ad020fc771b299d0b208f17e9b58
zhdenny <dennyhz@gmail.com>
2026-06-09 20:17:04 -0400

    unraid config fix

--- nvim_unraid/lua/vim-options.lua
+++ nvim_unraid/lua/vim-options.lua
@@ -176,6 +176,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank()
+		if vim.hl and vim.hl.hl_op then
+			vim.hl.hl_op()
+		else
+			(vim.hl or vim.highlight).on_yank()
+		end
 	end,
 })
