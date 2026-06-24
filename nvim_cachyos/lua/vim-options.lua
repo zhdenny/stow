149dbfd5e2d6b4fe420e0c3ad3b05558f118c3e5
zhdenny <dennyhz@gmail.com>
2026-06-23 22:41:34 -0400

    init cachyos

---
+++ nvim_cachyos/lua/vim-options.lua
@@ -0,0 +1,13 @@
+-- Sync clipboard between OS and Neovim via OSC 52.
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	},
+}
+vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
